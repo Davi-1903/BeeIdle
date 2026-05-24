58274be7bf0d618ed056f0315939b9a384c1e608
Davi Francisco <franciscodavi327@gmail.com>
2026-05-24 12:48:17 -0300

    feat: criar estrutura básica para o layout do editor

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer
+from textual.containers import Horizontal, Vertical
+from textual.widgets import Header, Footer, TabbedContent
 
 
 class Tidle(App):
+    DEFAULT_CSS = '''
+    #nav {
+        width: 2fr;
+    }
+
+    #code-container {
+        width: 8fr;
+        height: 100%;
+    }
+
+    .code:focus {
+        border: none;
+    }
+    '''
+
     def compose(self) -> ComposeResult:
         yield Header(show_clock=True)
+        with Horizontal():
+            yield Vertical(id='nav')
+            yield TabbedContent(id='code-container')
         yield Footer()
 
 
